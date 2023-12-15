0458ecc27777986a6a038a88b9526666c86a1fe2
Amelia Hernández <46725868+ameliahernandez@users.noreply.github.com>
2023-12-15 13:24:10 +0100

    Der Code war vorher zu komplex. Die neue Version ist leichter und funktioniert sehr gut.

--- main.ts
+++ main.ts
@@ -1,64 +1,48 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (Werkzeug == 0) {
+    if (Werkzeug == 1) {
         if (receivedNumber == 1) {
-            GewinnerR += 1
-        }
-        if (receivedNumber == 2) {
-            Gewinner += 1
-        }
-    } else {
-        if (Werkzeug == 1) {
-            if (receivedNumber == 0) {
-                Gewinner += 1
-            }
-            if (receivedNumber == 2) {
-                GewinnerR += 1
-            }
+            basic.showIcon(IconNames.Asleep)
+        } else if (receivedNumber == 2) {
+            basic.showIcon(IconNames.Sad)
         } else {
-            if (Werkzeug == 2) {
-                if (receivedNumber == 0) {
-                    GewinnerR += 1
-                }
-                if (receivedNumber == 1) {
-                	
-                }
-            }
+            basic.showIcon(IconNames.Happy)
+        }
+    } else if (Werkzeug == 2) {
+        if (receivedNumber == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else if (receivedNumber == 2) {
+            basic.showIcon(IconNames.Asleep)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
+    } else if (Werkzeug == 3) {
+        if (receivedNumber == 1) {
+            basic.showIcon(IconNames.Sad)
+        } else if (receivedNumber == 2) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Asleep)
         }
     }
-})
-input.onGesture(Gesture.LogoUp, function () {
-    if (Gewinner > GewinnerR) {
-        basic.showNumber(Gewinner)
-        basic.showString(control.deviceName())
-    } else {
-        basic.showString("Anderer Gewinner")
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    Funkgruppe += 1
-})
-input.onGesture(Gesture.Shake, function () {
-    Werkzeug = randint(0, 2)
-    radio.sendNumber(Werkzeug)
-    if (Werkzeug == 0) {
-        basic.showIcon(IconNames.No)
-    } else if (Werkzeug == 1) {
-        basic.showIcon(IconNames.SmallDiamond)
-    } else {
-        basic.showIcon(IconNames.Square)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(Funkgruppe)
 })
 input.onButtonPressed(Button.B, function () {
-    Funkgruppe += -1
+    Werkzeug = randint(1, 3)
+    if (Werkzeug == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else if (Werkzeug == 2) {
+        basic.showIcon(IconNames.SmallDiamond)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    basic.pause(2000)
+    radio.sendNumber(Werkzeug)
 })
 // Kinder können sich eine Funkgruppe aussuchen
 let Werkzeug = 0
-let GewinnerR = 0
-let Gewinner = 0
-let Funkgruppe = 0
-Funkgruppe = 1
-Gewinner = 0
-GewinnerR = 0
+radio.setGroup(2)
